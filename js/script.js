d69f2d90734207d32cbbc7150f510a79ac724104
Melozzo <carmecamma1995@gmail.com>
2023-09-12 16:33:46 +0200

    [edit]: control email in js

--- js/script.js
+++ js/script.js
@@ -6,19 +6,30 @@ btnSubmit.addEventListener('click', function (){
   // VALUE EMAIL
   let valueEmail = addEmail.value;
   console.log(valueEmail);
-  let samEmail = false
 
   // LIST
-  const listEmail= ['cincinnati@gmail.com','batticinque@gmail.com','sonomagico7@gmail.com']
+  const listEmail = ['cincinnati@gmail.com', 'batticinque@gmail.com', 'sonomagico7@gmail.com']
 
   // 'cillumini99@gmail.com','pensapete@gmail.com','papete2000@gmail.com','spidermanreal.98@gmail.com','alloroelimone@gmail.com','solosoletto@gmail.com','carriola58246@gmail.com','hakerpercaso007@gmail.com','ciattacchilachiave@gmail.com','otellorottello@gmail.com'
-  
-  // CONTROL
-  for (let i=0; i < listEmail.length; i++){
-    const emailControl = list.listEmail[i];
 
-    if (addEmail === emailControl )
-    samEmail=true
+  // CONTROL
+  let samEmail = false
+
+  for (let i=0; i < listEmail.length; i++){
+    const emailControl = listEmail[i];
+
+    if (valueEmail === emailControl){
+      samEmail = true
+    }
+    let dice = document.getElementById('game-dice')
+    if (samEmail ===true){
+      dice.innerHTML =`Benvenuto nel gioco, tira il tuo dado`;
+    } else {
+      dice.innerHTML =`Inserire email corretta`;
+    }
+
+
+
 
   }
 
