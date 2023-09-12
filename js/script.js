dd26fe04fd5fe56b7394711293613bc54585c557
Melozzo <carmecamma1995@gmail.com>
2023-09-12 15:38:20 +0200

    [edit]: const list email

---
+++ js/script.js
@@ -0,0 +1,1 @@
+const listEmail= ['cincinnati@gmail.com','batticinque@gmail.com','sonomagico7@gmail.com','cillumini99@gmail.com','pensapete@gmail.com','papete2000@gmail.com','spidermanreal.98@gmail.com','alloroelimone@gmail.com','solosoletto@gmail.com','carriola58246@gmail.com','hakerpercaso007@gmail.com','ciattacchilachiave@gmail.com','otellorottello@gmail.com']
