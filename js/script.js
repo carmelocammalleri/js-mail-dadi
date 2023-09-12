9b5a28aeba98c5755bbd3340ab591b78326f4934
Melozzo <carmecamma1995@gmail.com>
2023-09-12 17:35:33 +0200

    [fix]: bugs in js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ btnSubmit.addEventListener('click', function (){
   // 'cillumini99@gmail.com','pensapete@gmail.com','papete2000@gmail.com','spidermanreal.98@gmail.com','alloroelimone@gmail.com','solosoletto@gmail.com','carriola58246@gmail.com','hakerpercaso007@gmail.com','ciattacchilachiave@gmail.com','otellorottello@gmail.com'
 
   // CONTROL
-  let samEmail = false
+  let samEmail = false;
 
   for (let i=0; i < listEmail.length; i++){
     const emailControl = listEmail[i];
@@ -22,25 +22,29 @@ btnSubmit.addEventListener('click', function (){
       samEmail = true
     }
     
-    let dice = document.getElementById('game-dice')
+    let dice = document.getElementById('game-dice');
+    const roll = document.getElementById('tira');
+
+    // GAME DICE
     if (samEmail ===true){
-      dice.innerHTML = `Benvenuto nel gioco, tira il tuo dado`;  
-      const roll = document.getElementById ('shoot')
-      roll.classList.remove('d-none');
+      dice.innerHTML = `Benvenuto nel gioco, tira il tuo dado`;
+      roll.classList.remove('d-none')
+
     } else {
       dice.innerHTML =`Inserire email corretta`;
     }
-
-
-
-
+    
   }
-
-
-
-
-
+  
 })
+
+let 
+const min= 1;
+const max= 6;
+const random = Math.floor(Math.random()*(max - min +1))+ min;
+console.log(random);   
+
+
 
 
 
