436a69f08c5f2bac747808eb0d383b4852f39139
Melozzo <carmecamma1995@gmail.com>
2023-09-12 16:13:48 +0200

    [fix]: btn section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+const btnSubmit = document.querySelector('.submit');
+const addEmail = document.getElementById('email');
+
+btnSubmit.addEventListener('click', function (){
+  let valueEmail = addEmail.value;
+  console.log(valueEmail);
+})
+
+
+
 const listEmail= ['cincinnati@gmail.com','batticinque@gmail.com','sonomagico7@gmail.com']
 
 // 'cillumini99@gmail.com','pensapete@gmail.com','papete2000@gmail.com','spidermanreal.98@gmail.com','alloroelimone@gmail.com','solosoletto@gmail.com','carriola58246@gmail.com','hakerpercaso007@gmail.com','ciattacchilachiave@gmail.com','otellorottello@gmail.com'
 
-const addEmail = document.querySelector('form-control')
+
 
 let samEmail = false
-for(let i=0; i < listEmail.length; i++ ){
-  
+
+for (let i=0; i < listEmail.length; i++){
+  const emailControl = list.listEmail[i]
+  if (addEmail === emailControl )
+  samEmail=true
+
 }
