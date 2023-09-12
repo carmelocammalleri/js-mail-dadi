4279aa4f13ebf5f6fcbb36433a75ebea8c6c79ab
Melozzo <carmecamma1995@gmail.com>
2023-09-12 15:45:19 +0200

    [edit]: addEmail const and flag

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const listEmail= ['cincinnati@gmail.com','batticinque@gmail.com','sonomagico7@gmail.com','cillumini99@gmail.com','pensapete@gmail.com','papete2000@gmail.com','spidermanreal.98@gmail.com','alloroelimone@gmail.com','solosoletto@gmail.com','carriola58246@gmail.com','hakerpercaso007@gmail.com','ciattacchilachiave@gmail.com','otellorottello@gmail.com']
+const listEmail= ['cincinnati@gmail.com','batticinque@gmail.com','sonomagico7@gmail.com']
+
+// 'cillumini99@gmail.com','pensapete@gmail.com','papete2000@gmail.com','spidermanreal.98@gmail.com','alloroelimone@gmail.com','solosoletto@gmail.com','carriola58246@gmail.com','hakerpercaso007@gmail.com','ciattacchilachiave@gmail.com','otellorottello@gmail.com'
+
+const addEmail = document.querySelector('form-control')
+
+let samEmail = false
+for(let i=0; i < listEmail.length; i++ ){
+  
+}
